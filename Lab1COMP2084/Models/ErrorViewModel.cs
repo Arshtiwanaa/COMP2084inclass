c0cdfc561ff6e90da290fb3e17fc605c230d439b
arshtiwana <200489562@student.georgianc.on.ca>
2022-05-18 19:13:56 -0400

    intial commit

---
+++ Lab1COMP2084/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Lab1COMP2084.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
